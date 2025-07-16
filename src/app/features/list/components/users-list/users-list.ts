3c2738a9ac5162c9ceb154ceae7f074ccc549eed
Gabriel Meira <gabrielcervalmeira@gmail.com>
2025-07-16 10:37:25 -0300

    Melhoria no layout da listagem utilizando o mat-card

--- src/app/features/list/components/users-list/users-list.ts
+++ src/app/features/list/components/users-list/users-list.ts
@@ -1,11 +1,14 @@
 import { Component, input, output } from '@angular/core';
 import { User } from '../../../../shared/interfaces/user';
+import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
 
 
 @Component({
   selector: 'app-users-list',
   templateUrl: './users-list.html',
-  styleUrl: './users-list.scss'
+  styleUrl: './users-list.scss',
+  imports: [MatCardModule, MatButtonModule]
 })
 export class UsersList {
   users = input.required<User[]>();
