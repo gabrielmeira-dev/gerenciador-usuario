9c0c4b7f0daad4528ce24b1548f1568f2d19ce5a
Gabriel Meira <gabrielcervalmeira@gmail.com>
2025-07-16 12:59:13 -0300

    Estilização da pagina de create usando o angular Material

--- src/app/features/create/create.ts
+++ src/app/features/create/create.ts
@@ -1,11 +1,14 @@
 import { Component, inject } from '@angular/core';
 import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { Users } from '../../shared/services/users';
-import { Router } from '@angular/router';
+import { Router, RouterLink } from '@angular/router';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-create',
-  imports: [ReactiveFormsModule],
+  imports: [ReactiveFormsModule, MatFormFieldModule, MatInputModule, MatButtonModule, RouterLink],
   templateUrl: './create.html',
   styleUrl: './create.scss'
 })
