507e260fce787772090044387b4b27a949db9ff1
Gabriel Meira <gabrielcervalmeira@gmail.com>
2025-07-24 08:58:53 -0300

    Uso de pipe no nome do usuario

--- src/app/features/list/components/users-list/users-list.ts
+++ src/app/features/list/components/users-list/users-list.ts
@@ -3,13 +3,14 @@ import { User } from '../../../../shared/interfaces/user';
 import { MatCardModule } from '@angular/material/card';
 import { MatButtonModule } from '@angular/material/button';
 import { ErrorBtn } from './directives/error-btn/error-btn';
+import { TitleCasePipe } from '@angular/common';
 
 
 @Component({
   selector: 'app-users-list',
   templateUrl: './users-list.html',
   styleUrl: './users-list.scss',
-  imports: [MatCardModule, MatButtonModule, ErrorBtn]
+  imports: [MatCardModule, MatButtonModule, ErrorBtn, TitleCasePipe]
 })
 export class UsersList {
   users = input.required<User[]>();
